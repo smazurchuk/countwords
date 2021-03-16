202815fe7be03ace45090bc8fa04ae96edfd28cf
Stephen Mazurchuk <stephen.mazurchuk@gmail.com>
2021-03-16 17:17:03 -0500

    updated benchmark to have jitted python version

--- benchmark.py
+++ benchmark.py
@@ -43,6 +43,7 @@ programs = [
     ('OCaml', './simple-ml', None, 'by Nate Dobbins'),
     ('Kotlin', 'java -jar simple-kotlin.jar', None, 'by Kazik Pogoda'),
     ('Lua', 'luajit simple.lua', 'luajit optimized.lua', 'by themadsens; runs under luajit'),
+    ('Python - Jitted', 'python3 simple_jit.py', '', ''),
 ]
 
 times = []
